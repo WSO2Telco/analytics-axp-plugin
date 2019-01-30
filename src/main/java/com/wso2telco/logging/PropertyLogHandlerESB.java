24030d94ca4b7f95562abb7cf195a3ac6f1a0472
Sheshan <seshangamage@gmail.com>
2019-01-30 12:04:39 +0530

    Modification to log to sperate file for ESB datapublishing using Logstash

--- src/main/java/com/wso2telco/logging/PropertyLogHandlerESB.java
+++ src/main/java/com/wso2telco/logging/PropertyLogHandlerESB.java
@@ -1,12 +1,10 @@
 package com.wso2telco.logging;
 
-import org.apache.axiom.om.impl.llom.OMTextImpl;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.impl.Log4JLogger;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.commons.json.JsonUtil;
-import org.apache.synapse.config.Entry;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
 import org.json.JSONObject;
@@ -15,6 +13,7 @@ import org.json.XML;
 public class PropertyLogHandlerESB extends AbstractMediator{
 
     Log logHandler = LogFactory.getLog("REQUEST_RESPONSE_LOGGER");
+	Log billingLogHandler = LogFactory.getLog("BILLING_REQUEST_RESPONSE_LOGGER");
     private static final String DIRECTION = "DIRECTION";
     private static final String APPLICATION_ID = "APPLICATION_ID";
     private static final String REQUESTID = "REQUEST_ID";
@@ -25,8 +24,12 @@ public class PropertyLogHandlerESB extends AbstractMediator{
     private static final String RESPONSE_TIME= "RESPONSE_TIME";
     private static final String OPERATOR_NAME = "OPERATOR_NAME";
     private static final String OPERATOR_ID = "OPERATOR_ID";
+    private static final String API_ID = "API_ID";
+    private static final String APPLICATION_NAME = "APPLICATION_NAME";
+    private static final String API_PUBLISHER = "API_PUBLISHER";
+    private static final String REST_METHOD = "REST_METHOD";
     private static String FAULT = null;
-    private static String API_OPERATION = null;
+
 
     public boolean mediate(MessageContext messageContext) {
 
@@ -111,6 +114,21 @@ public class PropertyLogHandlerESB extends AbstractMediator{
                     "-wso2telco_value,OPERATOR_ID:wso2telco_value:" + messageContext.getProperty(OPERATOR_ID) +
                     "-wso2telco_value,Body:wso2telco_value:" + jsonBody.replaceAll("\n", "") +
                     "-wso2telco_value,ERROR:" + FAULT);
+            billingLogHandler.info("NORTHBOUND_RESPONSE_LOGGER-"+"API_REQUEST_ID:wso2telco_value:" +  messageContext.getProperty(REQUESTID) +
+                    "-wso2telco_value,API:wso2telco_value:" + messageContext.getProperty(API_NAME) +
+                    "-wso2telco_value,RESOURCE_PATH:wso2telco_value:" + messageContext.getProperty(RESOURCE) +
+                    "-wso2telco_value,METHOD:wso2telco_value:" + messageContext.getProperty(REST_METHOD) +
+                    "-wso2telco_value,RESPONSE_TIME:wso2telco_value:" + messageContext.getProperty(RESPONSE_TIME)+
+                    "-wso2telco_value,API_PUBLISHER:wso2telco_value:" + messageContext.getProperty(API_PUBLISHER) +
+                    "-wso2telco_value,APPLICATION_NAME:wso2telco_value:" + messageContext.getProperty(APPLICATION_NAME) +
+                    "-wso2telco_value,REQUEST_ID:wso2telco_value:" + messageContext.getProperty(REQUESTID) +
+                    "-wso2telco_value,DIRECTION:wso2telco_value:" + messageContext.getProperty(DIRECTION) +
+                    "-wso2telco_value,JSON_BODY:wso2telco_value:" + jsonBody.replaceAll("\\s", "") +
+                    "-wso2telco_value,API_ID:wso2telco_value:" + messageContext.getProperty(API_ID) +
+                    "-wso2telco_value,APPLICATION_ID:wso2telco_value:" + messageContext.getProperty(APPLICATION_ID) +
+                    "-wso2telco_value,OPERATOR_NAME:wso2telco_value:" + messageContext.getProperty(OPERATOR_NAME) +
+                    "-wso2telco_value,OPERATOR_ID:wso2telco_value:" + messageContext.getProperty(OPERATOR_ID) +
+                    "-wso2telco_value,SP_USER_ID:wso2telco_value:" + messageContext.getProperty(USER_ID));
         }
         else if(direction.equals("sb response")){
             logHandler.info("SOUTHBOUND_RESPONSE_LOGGER-"+"API_REQUEST_ID:wso2telco_value:" + messageContext.getProperty(REQUESTID) +
@@ -126,6 +144,21 @@ public class PropertyLogHandlerESB extends AbstractMediator{
                     "-wso2telco_value,OPERATOR_ID:wso2telco_value:" + messageContext.getProperty(OPERATOR_ID) +
                     "-wso2telco_value,Body:wso2telco_value:" + jsonBody.replaceAll("\n", "") +
                     "-wso2telco_value,ERROR:" + FAULT);
+            billingLogHandler.info("SOUTHBOUND_RESPONSE_LOGGER-"+"API_REQUEST_ID:wso2telco_value:" +  messageContext.getProperty(REQUESTID) +
+                    "-wso2telco_value,API:wso2telco_value:" + messageContext.getProperty(API_NAME) +
+                    "-wso2telco_value,RESOURCE_PATH:wso2telco_value:" + messageContext.getProperty(RESOURCE) +
+                    "-wso2telco_value,METHOD:wso2telco_value:" + messageContext.getProperty(REST_METHOD) +
+                    "-wso2telco_value,RESPONSE_TIME:wso2telco_value:" + messageContext.getProperty(RESPONSE_TIME)+
+                    "-wso2telco_value,API_PUBLISHER:wso2telco_value:" + messageContext.getProperty(API_PUBLISHER) +
+                    "-wso2telco_value,APPLICATION_NAME:wso2telco_value:" + messageContext.getProperty(APPLICATION_NAME) +
+                    "-wso2telco_value,REQUEST_ID:wso2telco_value:" + messageContext.getProperty(REQUESTID) +
+                    "-wso2telco_value,DIRECTION:wso2telco_value:" + messageContext.getProperty(DIRECTION) +
+                    "-wso2telco_value,JSON_BODY:wso2telco_value:" + jsonBody.replaceAll("\\s", "") +
+                    "-wso2telco_value,API_ID:wso2telco_value:" + messageContext.getProperty(API_ID) +
+                    "-wso2telco_value,APPLICATION_ID:wso2telco_value:" + messageContext.getProperty(APPLICATION_ID) +
+                    "-wso2telco_value,OPERATOR_NAME:wso2telco_value:" + messageContext.getProperty(OPERATOR_NAME) +
+                    "-wso2telco_value,OPERATOR_ID:wso2telco_value:" + messageContext.getProperty(OPERATOR_ID) +
+                    "-wso2telco_value,SP_USER_ID:wso2telco_value:" + messageContext.getProperty(USER_ID));
 
         }
     }
