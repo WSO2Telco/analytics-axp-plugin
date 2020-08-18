c54f3399f54637399d5164358bd9da0991a4cd5b
rajitha93 <rajithadrck@gmail.com>
2020-08-18 21:44:10 +0530

    [ANLT-1097] - Add property to read some of the constant values

--- src/main/java/com/wso2telco/util/Configurations.java
+++ src/main/java/com/wso2telco/util/Configurations.java
@@ -4,45 +4,56 @@ import java.util.Properties;
 
 public final class Configurations {
 
-    private Properties properties = null;
     private static Configurations instance = null;
+    private Properties properties = null;
 
-    /** Private constructor */
-    private Configurations (){
+    /**
+     * Private constructor
+     */
+    private Configurations() {
         this.properties = new Properties();
-        try{
+        try {
             properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(Constants.PATH_CONFFILE));
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    /** Creates the instance is synchronized to avoid multithreads problems */
-    private synchronized static void createInstance () {
+    /**
+     * Creates the instance is synchronized to avoid multithreads problems
+     */
+    private synchronized static void createInstance() {
         if (instance == null) {
-            instance = new Configurations ();
+            instance = new Configurations();
         }
     }
 
-    /** Get the properties instance. Uses singleton pattern */
-    public static Configurations getInstance(){
+    /**
+     * Get the properties instance. Uses singleton pattern
+     */
+    public static Configurations getInstance() {
         // Uses singleton pattern to guarantee the creation of only one instance
-        if(instance == null) {
+        if (instance == null) {
             createInstance();
         }
         return instance;
     }
 
-    /** Get a property of the property file */
-    public String getProperty(String key){
+    /**
+     * Get a property of the property file
+     */
+    public String getProperty(String key) {
         String result = null;
-        if(key !=null && !key.trim().isEmpty()){
+        if (key != null && !key.trim().isEmpty()) {
             result = this.properties.getProperty(key);
         }
         return result;
     }
 
-    /** Override the clone method to ensure the "unique instance" requirement of this class */
+    /**
+     * Override the clone method to ensure the "unique instance" requirement of this class
+     */
     public Object clone() throws CloneNotSupportedException {
         throw new CloneNotSupportedException();
-    }}
+    }
+}
--- src/main/java/com/wso2telco/util/Properties.java
+++ src/main/java/com/wso2telco/util/Properties.java
@@ -2,7 +2,7 @@ package com.wso2telco.util;
 
 public class Properties {
     // Properties (user configurable)
-    public static final String FILE_NAME="config.file.name";
+    public static final String FILE_NAME = "config.file.name";
     public static final String LOGMESSAGEDELIMITER = "log.message.delimeter";
     public static final String LOGDATADELIMITER = "log.data.delimeter";
     public static final String AM_MAPPING_ID = "am.mapping.id";
